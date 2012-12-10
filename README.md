2ef0102eb2f98abcffd05cb9a6d5e3037a6a6f25
Bruno Batista <bruno@joomlapro.com.br>
2012-12-10 14:36:03 -0200

    First Commit

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
-plg_user_password
-=================
-
 Password User Plugin for Joomla! 3.0
+====================================
+
+Simple plugin to check if user not logged in, if yes redirect to change the password.
+
+## Installation
+
+Just download and install.
+
+## Authors
+
+* Bruno Batista
+
+## License
+
+Licensed under the terms of the MIT license.
+
+## Bugs/Requests
+
+* You can [report a bug or request a feature here](http://github.com/joomlapro/plg_user_password/issues)
---
+++ password.php
@@ -0,0 +1,157 @@
+<?php
+/**
+ * @package     Joomla.Plugin
+ * @subpackage  User.password
+ * @copyright   Copyright (C) 2012 AtomTech, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// No direct access.
+defined('JPATH_BASE') or die;
+
+/**
+ * Password User plugin
+ *
+ * @package     Joomla.Plugin
+ * @subpackage  User.password
+ * @since       3.0
+ */
+class PlgUserPassword extends JPlugin
+{
+	/**
+	 * Constructor
+	 *
+	 * @param   object  &$subject  The object to observe
+	 * @param   array   $config    An array that holds the plugin configuration
+	 *
+	 * @access  protected
+	 * @since   3.0
+	 */
+	public function __construct(& $subject, $config)
+	{
+		parent::__construct($subject, $config);
+		$this->loadLanguage();
+		JFormHelper::addFieldPath(__DIR__ . '/fields');
+	}
+
+	/**
+	 * This method should handle any login logic and report back to the subject
+	 *
+	 * @param   array  $user     Holds the user data
+	 * @param   array  $options  Array holding options (remember, autoregister, group)
+	 *
+	 * @return  boolean  True on success.
+	 *
+	 * @since   3.0
+	 */
+	public function onUserLogin($user, $options = array())
+	{
+		// Initialiase variables.
+		$instance = $this->_getUser($user, $options);
+
+		// If _getUser returned an error, then pass it back.
+		if ($instance instanceof Exception)
+		{
+			return false;
+		}
+
+		// If the user is blocked, redirect with an error
+		if ($instance->get('block') == 1)
+		{
+			JError::raiseWarning('SOME_ERROR_CODE', JText::_('JERROR_NOLOGIN_BLOCKED'));
+			return false;
+		}
+
+		// Authorise the user based on the group information
+		if (!isset($options['group']))
+		{
+			$options['group'] = 'USERS';
+		}
+
+		// Check the user can login.
+		$result	= $instance->authorise($options['action']);
+
+		if (!$result)
+		{
+			JError::raiseWarning(401, JText::_('JERROR_LOGIN_DENIED'));
+			return false;
+		}
+
+		// Mark the user as logged in
+		$instance->set('guest', 0);
+
+		// Register the needed session variables
+		$session = JFactory::getSession();
+		$session->set('user', $instance);
+
+		// Check if user not logged in, if yes redirect to change the password.
+		$db = JFactory::getDBO();
+		$app = JFactory::getApplication();
+
+		if ($instance->get('lastvisitDate') == $db->getNullDate())
+		{
+			JError::raiseWarning(401, JText::_('PLG_USER_PASSWORD_MSG_CHANGE_PASSWORD'));
+			$app->redirect(JRoute::_('index.php?option=com_users&view=profile&layout=edit', false));
+		}
+
+		return true;
+	}
+
+	/**
+	 * This method will return a user object
+	 *
+	 * If options['autoregister'] is true, if the user doesn't exist yet he will be created
+	 *
+	 * @param   array  $user     Holds the user data.
+	 * @param   array  $options  Array holding options (remember, autoregister, group).
+	 *
+	 * @return  object  A JUser object.
+	 *
+	 * @since   3.0
+	 */
+	protected function _getUser($user, $options = array())
+	{
+		// Initialiase variables.
+		$instance = JUser::getInstance();
+		$id       = (int) JUserHelper::getUserId($user['username']);
+
+		if ($id)
+		{
+			$instance->load($id);
+			return $instance;
+		}
+
+		// TODO: move this out of the plugin
+		$config	= JComponentHelper::getParams('com_users');
+
+		// Default to Registered.
+		$defaultUserGroup = $config->get('new_usertype', 2);
+
+		$instance->set('id', 0);
+		$instance->set('name', $user['fullname']);
+		$instance->set('username', $user['username']);
+		$instance->set('password_clear', $user['password_clear']);
+
+		// Result should contain an email (check)
+		$instance->set('email', $user['email']);
+		$instance->set('groups', array($defaultUserGroup));
+
+		// If autoregister is set let's register the user
+		$autoregister = isset($options['autoregister']) ? $options['autoregister'] :  $this->params->get('autoregister', 1);
+
+		if ($autoregister)
+		{
+			if (!$instance->save())
+			{
+				return JError::raiseWarning('SOME_ERROR_CODE', $instance->getError());
+			}
+		}
+		else
+		{
+			// No existing user and autoregister off, this is a temporary user.
+			$instance->set('tmp_user', true);
+		}
+
+		return $instance;
+	}
+}
